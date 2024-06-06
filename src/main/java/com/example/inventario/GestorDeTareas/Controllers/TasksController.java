39d5cba07102a6f3a0ba6558b453373938f0f6ae
Juan Cardona <jcardonabageth@gmail.com>
2024-06-06 16:47:45 -0500

    feat: add new methods for task (update, delete and get all task for id user)

--- src/main/java/com/example/inventario/GestorDeTareas/Controllers/TasksController.java
+++ src/main/java/com/example/inventario/GestorDeTareas/Controllers/TasksController.java
@@ -14,14 +14,26 @@ public class TasksController {
     @Autowired
     private TaskService taskService;
 
-    @GetMapping(value = "/task")
-    public ResponseEntity<String> hi() {
-        return ResponseEntity.ok("Hola");
+    @GetMapping(value = "/user/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getAllTaskByUserId(@PathVariable(value = "userId") long userId) {
+        return taskService.getAllTaskByUserId(userId);
     }
 
     @PostMapping(value = "/user/{userId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createTask(@RequestBody TaskDTO taskDTO, @PathVariable(value = "userId") long userId) {
         return taskService.createNewTask(taskDTO, userId);
     }
+
+    @PutMapping(value = "/user/{userId}/task/{taskId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updateTask(@RequestBody TaskDTO taskDTO, @PathVariable(value = "userId") long userId, @PathVariable(value = "taskId") long taskId) {
+        return taskService.updateTask(taskDTO, userId, taskId);
+    }
+
+    @DeleteMapping(value = "/user/{userId}/task/{taskId}")
+    public ResponseEntity<?> deleteTask(@PathVariable(value = "userId") long userId, @PathVariable(value = "taskId") long taskId) {
+        return taskService.deleteTask(userId, taskId);
+    }
+
+
 
 }
--- src/main/java/com/example/inventario/GestorDeTareas/Repositories/TasksRepositorie.java
+++ src/main/java/com/example/inventario/GestorDeTareas/Repositories/TasksRepositorie.java
@@ -1,9 +1,15 @@
 package com.example.inventario.GestorDeTareas.Repositories;
 
 import com.example.inventario.GestorDeTareas.Models.TaskModel;
+import org.hibernate.validator.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TasksRepositorie extends JpaRepository<TaskModel, Long> {
+
+    List<TaskModel> getAllTaskByUserid(Long userId);
+
 }
